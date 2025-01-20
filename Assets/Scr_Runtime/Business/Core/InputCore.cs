59e705d32c1d570c7cc4ad05e4d6a4cfb07fbb10
DogCat_Shike <dcsk.flzj@gmail.com>
2025-01-21 02:11:26 +0800

    连接Minis输入，现在是GetKey，得把bool部分改成GetKeyDown

--- Assets/Scr_Runtime/Business/Core/InputCore.cs
+++ Assets/Scr_Runtime/Business/Core/InputCore.cs
@@ -1,15 +1,50 @@
 using System;
 using UnityEngine;
 using HNY;
+using UnityEngine.InputSystem;
 
 public class InputCore
 {
-    public bool GetKeyDown()
+    public bool getKeyDown;
+    public void GetKey()
     {
-        if(Input.anyKeyDown)
+        //onDeviceChange 输入设备变化触发事件
+        InputSystem.onDeviceChange += (device, change) =>
         {
-            return true;
-        }
-        return false;
+            //Added 添加新设备
+            if (change != InputDeviceChange.Added) return;
+
+            var midiDevice = device as Minis.MidiDevice;
+            if (midiDevice == null) return;
+
+            midiDevice.onWillNoteOn += (note, velocity) => {
+                // Debug.Log(string.Format(
+                //     "Note On #{0} ({1}) vel:{2:0.00} ch:{3} dev:'{4}'",
+                //     note.noteNumber,
+                //     note.shortDisplayName,
+                //     velocity,
+                //     (note.device as Minis.MidiDevice)?.channel,
+                //     note.device.description.product
+                // ));
+                
+                getKeyDown = true;
+
+                //TODO: 播放音频
+            };
+
+            midiDevice.onWillNoteOff += (note) => {
+                // Debug.Log(string.Format(
+                //     "Note Off #{0} ({1}) ch:{2} dev:'{3}'",
+                //     note.noteNumber,
+                //     note.shortDisplayName,
+                //     (note.device as Minis.MidiDevice)?.channel,
+                //      note.device.description.product
+                // ));
+
+                getKeyDown = false;
+
+                //TODO: 停止音频播放(渐弱)
+            };
+        };
     }
 }
--- Assets/Scr_Runtime/Main.cs
+++ Assets/Scr_Runtime/Main.cs
@@ -18,7 +18,8 @@ namespace HNY
 
         void Update()
         {
-            bool isKey = ctx.inputCore.GetKeyDown();
+            ctx.inputCore.GetKey();
+            bool isKey = ctx.inputCore.getKeyDown;
             float dt = Time.deltaTime;
 
             if(isKey)
@@ -29,44 +30,5 @@ namespace HNY
 
             GameBusiness.Tick(ctx, dt);
         }
-
-        #region test Minis
-        void Start()
-        {
-            InputSystem.onDeviceChange += (device, change) =>
-            {
-                if (change != InputDeviceChange.Added) return;
-
-                var midiDevice = device as Minis.MidiDevice;
-                if (midiDevice == null) return;
-
-                midiDevice.onWillNoteOn += (note, velocity) => {
-                    // Note that you can't use note.velocity because the state
-                    // hasn't been updated yet (as this is "will" event). The note
-                    // object is only useful to specify the target note (note
-                    // number, channel number, device name, etc.) Use the velocity
-                    // argument as an input note velocity.
-                    Debug.Log(string.Format(
-                        "Note On #{0} ({1}) vel:{2:0.00} ch:{3} dev:'{4}'",
-                        note.noteNumber,
-                        note.shortDisplayName,
-                        velocity,
-                        (note.device as Minis.MidiDevice)?.channel,
-                        note.device.description.product
-                    ));
-                };
-
-                midiDevice.onWillNoteOff += (note) => {
-                    Debug.Log(string.Format(
-                        "Note Off #{0} ({1}) ch:{2} dev:'{3}'",
-                        note.noteNumber,
-                        note.shortDisplayName,
-                        (note.device as Minis.MidiDevice)?.channel,
-                        note.device.description.product
-                    ));
-                };
-            };
-        }
-        #endregion
     }
 }
