d2dce873f94509206e58684267a770fde125d2f8
DogCat_Shike <dcsk.flzj@gmail.com>
2025-01-28 13:30:17 +0800

    <WIP> 存一下这个思路

--- Assets/Scr_Runtime/Business/Entity/FootEntity.cs
+++ Assets/Scr_Runtime/Business/Entity/FootEntity.cs
@@ -8,17 +8,29 @@ public class FootEntity : MonoBehaviour
     public IDSignature idSig;
 
     public GameObject[] foots;
+    public int[] maxCopy;
+    public int[] copyNumbers;
+    public float[] moveDist;
+    public float[] footYPos;
 
-    int moveSpeed;
+    public float moveSpeed;
 
     public void Ctor()
     {
         foots = new GameObject[9];
+        maxCopy = new int[9];
+        copyNumbers = new int[9];
+        moveDist = new float[9];
+        footYPos = new float[9];
         moveSpeed = 10;
 
         for (int i = 0; i < foots.Length; i++)
         {
             foots[i] = transform.Find("Foot" + (i+1)).gameObject;
+
+            maxCopy[i] = UnityEngine.Random.Range(2, 8);
+            copyNumbers[i] = 0;
+            moveDist[i] = 0;
         }
     }
 
@@ -44,14 +56,66 @@ public class FootEntity : MonoBehaviour
             int j = i / 2;
             j = 6 - j;
             foots[i].transform.position = heads[j].transform.position;
+
+            footYPos[i] = foots[i].transform.position.y;
         }
 
         foots[0].transform.position = (heads[7].transform.position + heads[6].transform.position) / 2;
+        footYPos[0] = foots[0].transform.position.y;
     }
 
     public void Move(float dt)
     {
+        // for (int i = 0; i < foots.Length; i++)
+        // {
+        //     if (copyNumbers[i] < randomNumbers[i])
+        //     {
+        //         float y = transform.position.y;
+        //         float maxMove = Mathf.Lerp(5, 1, (randomNumbers[i] - 2) / 6);
 
+        //         if (y <= footYPos[i] - maxMove)
+        //         {
+        //             float randomX = UnityEngine.Random.Range(-1f, 1f);
+
+        //             Vector3 newPos = new Vector3(randomX * dt, moveSpeed * dt);
+        //             transform.position += newPos;
+        //         }
+        //         else
+        //         {
+        //             copyNumbers[i]++;
+        //             CopySelf();
+        //         }
+        //     }
+        // }
+
+        for (int i = 0; i < foots.Length; i++)
+        {
+            if (copyNumbers[i] < maxCopy[i])
+            {
+                float maxMove = Mathf.Lerp(5, 1, (maxCopy[i] - 2) / 6);
+
+                float randomX = UnityEngine.Random.Range(-1f, 1f);
+                foots[i].transform.position -= new Vector3(randomX * dt, moveSpeed * dt);
+                moveDist[i] += moveSpeed * dt;
+
+                if(moveDist[i] >= footYPos[i] - maxMove)
+                {
+                    CopySelf();
+                    moveDist[i] = 0;
+                }
+            }
+        }
+    }
+
+    public void CopySelf()
+    {
+        GameObject foot = Instantiate(gameObject, transform.position, transform.rotation);
+        foot.GetComponent<FootEntity>().copyNumbers = (int[])copyNumbers.Clone();
+        
+        for (int i = 0; i < copyNumbers.Length; i++)
+        {
+            copyNumbers[i]++;
+        }
     }
 
     public void TearDown()
